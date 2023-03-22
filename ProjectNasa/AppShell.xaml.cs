83b1f3e14f870abdcaddd79e1669b37df8cb34aa
FrietStoofvlees <demonicskull0@gmail.com>
2023-03-22 22:22:32 +0100

    Add project files.

---
+++ ProjectNasa/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectNasa;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ProjectNasa/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProjectNasa;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
