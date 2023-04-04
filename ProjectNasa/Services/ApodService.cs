7bd0f07395427d9597a69db7c51e81c2b1f9b0ba
FrietStoofvlees <demonicskull0@gmail.com>
2023-04-04 21:26:22 +0200

    Working Refreshview with scrollview inside -> bugged in previous versions see: https://github.com/dotnet/maui/issues/5772

--- ProjectNasa/Services/ApodService.cs
+++ ProjectNasa/Services/ApodService.cs
@@ -20,7 +20,7 @@ namespace ProjectNasa.Services
 
         public async Task<Apod> GetAstronomyPictureoftheDayAsync()
         {
-            HttpResponseMessage response = await httpClient.GetAsync("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+            HttpResponseMessage response = await httpClient.GetAsync("https://api.nasa.gov/planetary/apod?api_key=***REMOVED***");
 
             if (response.IsSuccessStatusCode)
             {
