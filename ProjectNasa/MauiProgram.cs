c50876d1ffbb478eb50822a868e0eb13057d00ec
FrietStoofvlees <demonicskull0@gmail.com>
2023-04-17 01:43:00 +0200

    Save HD picture to device

--- ProjectNasa/MauiProgram.cs
+++ ProjectNasa/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Storage;
+using Microsoft.Extensions.Logging;
 
 namespace ProjectNasa;
 
@@ -6,14 +8,26 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+		//var builder = MauiApp.CreateBuilder();
+		//builder
+		//	.UseMauiApp<App>()
+		//	.ConfigureFonts(fonts =>
+		//	{
+		//		fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+		//		fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+		//	});
+
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+            .UseMauiCommunityToolkit()
+            // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
@@ -22,6 +36,7 @@ public static class MauiProgram
 		builder.Services.AddSingleton<IApodService, ApodService>();
         builder.Services.AddSingleton<ApodViewModel>();
         builder.Services.AddSingleton<ApodPage>();
+        builder.Services.AddSingleton(FileSaver.Default);
 
         builder.Services.AddTransient<ImageViewModel>();
 		builder.Services.AddTransient<ImagePage>();
