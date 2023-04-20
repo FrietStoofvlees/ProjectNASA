b5ec9a1ec006ecfc862853746e5548b34de941c0
FrietStoofvlees <demonicskull0@gmail.com>
2023-04-20 17:55:56 +0200

    .gitignore test

--- ProjectNasa/Services/ApodService.cs
+++ ProjectNasa/Services/ApodService.cs
@@ -21,7 +21,7 @@ namespace ProjectNasa.Services
 
         public async Task<Apod> GetAstronomyPictureoftheDayAsync()
         {
-            HttpResponseMessage response = await httpClient.GetAsync("https://api.nasa.gov/planetary/apod?api_key=***REMOVED***");
+            HttpResponseMessage response = await httpClient.GetAsync("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
 
             if (response.IsSuccessStatusCode)
             {
