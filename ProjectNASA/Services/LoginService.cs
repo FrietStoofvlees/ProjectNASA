b1fb661b7b1b617227627d8c347c447f621efbde
FrietStoofvlees <demonicskull0@gmail.com>
2023-05-01 21:41:28 +0200

    LoginPage

---
+++ ProjectNASA/Services/LoginService.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectNASA.Services
+{
+    public class LoginService : ILoginService
+    {
+        public async Task<User> Login(string username, string password)
+        {
+            User user = new()
+            {
+                Username = username,
+                Password = password
+            };
+
+            return await Task.FromResult(user);
+            //await Toast.Make("Please fill in your username and password").Show();
+        }
+    }
+}
--- ProjectNasa/ViewModel/LoginPageViewModel.cs
+++ ProjectNasa/ViewModel/LoginPageViewModel.cs
@@ -1,15 +1,40 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace ProjectNasa.ViewModel
+namespace ProjectNASA.ViewModel
 {
     public partial class LoginPageViewModel : BaseViewModel
     {
+        readonly ILoginService loginService;
+
+        [ObservableProperty]
         string username;
+        [ObservableProperty]
         string password;
+
+        [RelayCommand]
+        public void Login()
+        {
+            if (string.IsNullOrWhiteSpace(Username) || string.IsNullOrWhiteSpace(Password))
+            {
+                Toast.Make("Please fill in your username and password").Show();
+            } 
+            else
+            {
+                //User user = await loginService.Login(Username, Password);
+                //App.user = user;
+
+                ((AppShell)Application.Current.MainPage).SwitchtoTab(TabPages.ApodPage);
+            }
+        }
     }
 }
