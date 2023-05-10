382f3d44f22afb326a52099c0d5616da23e71340
FrietStoofvlees <demonicskull0@gmail.com>
2023-05-11 00:28:39 +0200

    WTIAPage

---
+++ ProjectNASA/ViewModel/WTIAViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectNASA.ViewModel
+{
+    public class WTIAViewModel : BaseViewModel
+    {
+        readonly IConnectivity connectivity;
+        readonly IWTIAService wTIAService;
+
+        [ObservableProperty]
+        ISS iss;
+
+        public WTIAViewModel(IWTIAService wTIAService, IConnectivity connectivity)
+        {
+            this.wTIAService = wTIAService;
+            this.connectivity = connectivity;
+        }
+
+
+    }
+}
