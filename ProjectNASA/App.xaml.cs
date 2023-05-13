ec246d200b241c823bcf5ee136a3eafbef39bfe3
FrietStoofvlees <demonicskull0@gmail.com>
2023-05-13 21:32:57 +0200

    Set the CursorColor on android for darkmode

--- ProjectNASA/App.xaml.cs
+++ ProjectNASA/App.xaml.cs
@@ -1,13 +1,25 @@
-﻿namespace ProjectNASA;
+﻿using Microsoft.Maui.Platform;
+
+namespace ProjectNASA;
 
 public partial class App : Application
 {
-	public static User User;
+    public static User User;
 
 	public App()
 	{
 		InitializeComponent();
 
 		MainPage = new AppShell();
-	}
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("CursorColor", (handler, view) =>
+        {
+#if ANDROID29_0_OR_GREATER
+            if (Current.RequestedTheme == AppTheme.Dark)
+            {
+                    handler.PlatformView.TextCursorDrawable.SetTint(Colors.White.ToPlatform());
+            }
+#endif
+        });
+    }
 }
