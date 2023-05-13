79f5e5d0d34beea65fcb3e57fd3b7fbceb2c8b7c
FrietStoofvlees <demonicskull0@gmail.com>
2023-05-13 21:32:24 +0200

    Added styles and class for adding behavior to entry in the style

---
+++ ProjectNASA/Library/MyEntryStyleBehavior.cs
@@ -0,0 +1,44 @@
+﻿using Maui.FixesAndWorkarounds;
+
+//https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/behaviors#consume-a-net-maui-behavior-with-a-style
+
+namespace ProjectNASA.Library
+{
+    public class MyEntryStyleBehavior : Behavior<Entry>
+    {
+        public static readonly BindableProperty AttachBehaviorProperty =
+            BindableProperty.CreateAttached("AttachBehavior", typeof(bool), typeof(MyEntryStyleBehavior), false, propertyChanged: OnAttachBehaviorChanged);
+
+        public static bool GetAttachBehavior(BindableObject view)
+        {
+            return (bool)view.GetValue(AttachBehaviorProperty);
+        }
+
+        public static void SetAttachBehavior(BindableObject view, bool value)
+        {
+            view.SetValue(AttachBehaviorProperty, value);
+        }
+
+        static void OnAttachBehaviorChanged(BindableObject view, object oldValue, object newValue)
+        {
+            if (view is not Entry entry)
+            {
+                return;
+            }
+
+            bool attachBehavior = (bool)newValue;
+            if (attachBehavior)
+            {
+                entry.Behaviors.Add(new TapToCloseBehavior());
+            }
+            else
+            {
+                Behavior toRemove = entry.Behaviors.FirstOrDefault(b => b is TapToCloseBehavior);
+                if (toRemove != null)
+                {
+                    entry.Behaviors.Remove(toRemove);
+                }
+            }
+        }
+    }
+}
