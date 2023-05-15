6bd666fd31bf11edbb6689222bf1e2341872728b
FrietStoofvlees <demonicskull0@gmail.com>
2023-05-15 20:40:22 +0200

    Add collectionview with swipeview for favorite images

--- ProjectNASA/ViewModel/GalleryPageViewModel.cs
+++ ProjectNASA/ViewModel/GalleryPageViewModel.cs
@@ -1,15 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace ProjectNASA.ViewModel
 {
-    public class GalleryPageViewModel : BaseViewModel
+    [QueryProperty(nameof(Apod), "Apod")]
+    public partial class GalleryPageViewModel : BaseViewModel
     {
-        public GalleryPageViewModel()
+        readonly IApodService apodService;
+
+        [ObservableProperty]
+        DateTime selectedDate;
+
+        public ObservableCollection<Apod> FavoriteApods { get; } = new();
+
+        Apod apod;
+        public Apod Apod 
         {
+            get { return apod; }
+            set
+            {
+                if (!FavoriteApods.Contains(value))
+                {
+                    FavoriteApods.Add(value);
+                }
+                apod = null;
+            }
+        }
+
+        public GalleryPageViewModel(IApodService apodService)
+        {
+            this.apodService = apodService;
+            SelectedDate = DateTime.Today;
+        }
+
+        [RelayCommand]
+        async Task GoToApodPageAsync()
+        {
+            //await Shell.Current.GoToAsync(nameof(ApodPage));
+            await Shell.Current.GoToAsync(nameof(ApodPage), true, new Dictionary<string, object>());
+        }
+
+        [RelayCommand]
+        void RemoveApod(Apod apod)
+        {
+            FavoriteApods.Remove(apod);
         }
     }
 }
